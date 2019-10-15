15f586a79b3025f8bc76f5f657bbf74ef0ecde11
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-10-15 09:50:37 +0200

    15. Operation Arguments

--- src/index.js
+++ src/index.js
@@ -3,34 +3,63 @@ import { GraphQLServer } from 'graphql-yoga'
 // Type definitions (schema)
 const typeDefs = `
     type Query {
+        greeting(name: String, position: String): String!
+        me: User!
+        post: Post!
+        add(n1: Float, n2: Float): Float!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
         title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        inStock: Boolean!
+        body: String!
+        published: Boolean!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        title() {
-            return 'Mi libro luna de pluton'
+        greeting(parent, args, ctx, info) {
+            if (args.name && args.position) {
+                return `Hello ${args.name} you are my favourite ${args.position}!`
+            } else {
+                return `Hello!`
+            }
         },
-        price() {
-            return 12.3
+        me() {
+            return {
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@gmail.com',
+                age: 28
+            }
         },
-        releaseYear() {
-            return null
+        post() {
+            return {
+                id: '123098',
+                title: 'How to get your driving license?',
+                body: 'Lorem dolor ipsum sit amen',
+                published: false
+            }
         },
-        rating() {
-            return 4.5
-        },
-        inStock() {
-            return true
+        add(undefined, args) {
+            if (args.n1 && args.n2) {
+                return args.n1 + args.n2
+            } else {
+                return 3.0
+            }
         }
     }
 }
+
 
 const server = new GraphQLServer({
     typeDefs,
